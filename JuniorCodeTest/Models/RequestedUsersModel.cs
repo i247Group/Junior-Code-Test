5d0bd2b9c23ab6ceae83eb43f894169aa350180f
Lewis Mutton <lewis.mutton@i247group.com>
2024-02-28 09:11:45 +0000

    Add initial base code, .gitignore & README.md

---
+++ JuniorCodeTest/Models/RequestedUsersModel.cs
@@ -0,0 +1,10 @@
+﻿namespace JuniorCodeTest.Models
+{
+	public class RequestedUsersModel
+	{
+		public string? Title { get; set; }
+		public string? First { get; set; }
+		public string? Last { get; set; }
+		public int Age { get; set; }
+	}
+}
